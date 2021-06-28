5aa0285fa08405c88b45480a81af54327b3d5637
Bjbajra <70744118+Bjbajra@users.noreply.github.com>
2021-06-28 10:46:12 +0200

    fix some bugs

--- WCE_App/ViewModels/AddCourseViewModel.cs
+++ WCE_App/ViewModels/AddCourseViewModel.cs
@@ -4,10 +4,15 @@ namespace WCE_App.ViewModels
 {
     public class AddCourseViewModel
     {
-        //[Required(ErrorMessage = "Course number is mandatory")]
+        [Required(ErrorMessage = "Course number is mandatory")]
+        [Display(Name = "Course number")]
         public int CourseNumber { get; set; }
+
+        [Required(ErrorMessage = "Title  is mandatory")]
         public string Title { get; set; }
         public string Description { get; set; }
+
+        [Required(ErrorMessage = "Please enter the duration")]
         public int Duration { get; set; }
         public string Level { get; set; }
         public string Status { get; set; }
